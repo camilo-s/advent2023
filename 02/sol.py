e8f0697709cf8fc9ee4c7c0e7c34535fb6a090b9
Camilo Sarmiento <csarmiento@thinkport.digital>
2023-12-04 13:16:37 +0100

    Add 2nd day solution part 2

--- 02/sol.py
+++ 02/sol.py
@@ -21,15 +21,24 @@ def check_draw_feasible(r=0, g=0, b=0):
 def check_game_feasible(game):
     game_content = game.split(":")[-1]
     draws = game_content.split(";")
+    game_max = {"r": 0, "g": 0, "b": 0}
+
     for draw in draws:
-        draw_dict = {}
         draw_strings = draw.split(",")
         for draw_string in draw_strings:
-            draw_dict.update(parse_draw(draw_string))
-        if not check_draw_feasible(**draw_dict):
-            return False
+            draw_dict = parse_draw(draw_string)
+            color = draw_dict["color"][0]
+            number = draw_dict["number"]
+            if draw_dict["number"] > game_max[color]:
+                draw_item = {color: number}
+                game_max.update(draw_item)
+    game_power = 1
+    for v in game_max.values():
+        game_power = game_power * v
+    if not check_draw_feasible(**game_max):
+        return [game_power, False]
     else:
-        return True
+        return [game_power, True]
 
 
 def parse_draw(draw_string):
@@ -37,16 +46,20 @@ def parse_draw(draw_string):
         r"(?P<number>[\d]+) (?P<color>(red|green|blue))", draw_string
     )
     draw_string_dict = draw_match.groupdict()
-    return {draw_string_dict["color"][0]: int(draw_string_dict["number"])}
+    draw_string_dict.update({"number": int(draw_string_dict["number"])})
+    return draw_string_dict
 
 
 def process_input(path):
     id_sum = 0
+    power_sum = 0
     for game in load_input(path):
         game_id = int(re.search(r"[\d]+", game).group())
-        if check_game_feasible(game):
+        game_power, game_feasible = check_game_feasible(game)
+        power_sum += game_power
+        if game_feasible:
             id_sum += game_id
-    return id_sum
+    return [id_sum, power_sum]
 
 
 print(process_input("input.txt"))
